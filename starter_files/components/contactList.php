e9628c5e648a2a32a78cabd0bca64da2680aae7d
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-02-28 16:41:23 +0100

    add contact list page

---
+++ starter_files/components/contactList.php
@@ -0,0 +1,124 @@
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <form class="d-flex">
+        <button class="btn btn-outline-danger" type="submit">Logout</button>
+      </form>
+    </div>
+  </nav>
+  <div class="container-md">
+    <div class="d-flex bd-highlight pt-5">
+      <div class="p-2 flex-grow-1 bd-highlight">
+        <h3>Contacts</h3>
+      </div>
+      <div class="p-2 bd-highlight pt-5 pb-5">
+        <div class="input-group">
+          <span class="input-group-text" id="basic-addon1">
+            <i class="bi bi-search"></i>
+          </span>
+          <input type="text" class="form-control" placeholder="Search...." aria-label="Search...."
+            aria-describedby="basic-addon1">
+        </div>
+      </div>
+      <div class="p-2 bd-highlight pt-5 pb-5">
+        <div class="input-group">
+          <button class="btn btn-outline-success" type="submit">
+            <i class="bi bi-person-plus"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body text-center">
+            </div>
+            <div class="table-responsive">
+              <table class="table">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">ID Number</th>
+                    <th scope="col">First name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Group</th>
+                    <th scope="col">Action</th>
+
+                  </tr>
+                </thead>
+                <tbody class="customtable">
+                  <tr>
+                    <td>01</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                  <tr>
+                    <td>02</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                  <tr>
+                    <td>03</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                  <tr>
+                    <td>04</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                  <tr>
+                    <td>05</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                  <tr>
+                    <td>06</td>
+                    <td>Bensaad</td>
+                    <td>Abdessamad</td>
+                    <td>bens@mail.com</td>
+                    <td>Street 12KL</td>
+                    <td>+212000000</td>
+                    <td><em>Family</em></td>
+                    <td><a href="#">Edit</a></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
