ca714b2de07f9ef96ec5a4bd2f54b8bcdbc3e78b
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-01 18:54:00 +0100

    change from home to index.php

--- starter_files/login.php
+++ starter_files/login.php
@@ -36,7 +36,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
                 $_SESSION['email'] = $row['email'];
                 $_SESSION['name'] = $row['name'];
                 $_SESSION['id'] = $row['id'];
-                header("Location: home.php");
+                header("Location: index.php");
                 exit();
             } else {
                 header("Location: index.php?error=Incorect User name or password");
