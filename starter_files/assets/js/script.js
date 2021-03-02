c8d07858b78fc953c856cbf8c39d75778d447d93
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-02 11:44:41 +0100

    search for person

--- starter_files/assets/js/script.js
+++ starter_files/assets/js/script.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
       $("[id$=test]").hide();
     });
 });
+
+var $rows = $("#table tr");
+$("#search").keyup(function () {
+  var val = $.trim($(this).val()).replace(/ +/g, " ").toLowerCase();
+
+  $rows
+    .show()
+    .filter(function () {
+      var text = $(this).text().replace(/\s+/g, " ").toLowerCase();
+      return !~text.indexOf(val);
+    })
+    .hide();
+});
