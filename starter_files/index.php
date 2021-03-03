66a833cd0259b415b861f42a69e0fdb850ff1673
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-03 10:04:08 +0100

    clean unused code index.php

--- starter_files/index.php
+++ starter_files/index.php
@@ -13,6 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+
+
 </head>
 
 <body>
@@ -20,7 +23,6 @@
     session_start();
     if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
         include('./components/contactList.php');
-        // include('./components/getdata.php');
 
     } else {
         include('./components/loginPage.php');
